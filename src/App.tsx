3a3ad2fc6d1c9d829b070036d4592c2421c565c2
OmniChain <omnichain@icloud.com>
2025-06-28 15:54:54 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import BlockDetail from "./pages/BlockDetail";
+import TxDetail from "./pages/TxDetail";
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="block/:height" element={<BlockDetail />} />
+        <Route path="tx/:hash" element={<TxDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
