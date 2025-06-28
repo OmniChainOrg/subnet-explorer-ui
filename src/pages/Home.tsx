ccedf5b773f92eca87b938efbafafe5b3402bde4
OmniChain <omnichain@icloud.com>
2025-06-28 15:53:08 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,1 +1,20 @@
+import { useEffect, useState } from "react";
+import { fetchLatestBlocks } from "../api/client";
+import { Link } from "react-router-dom";
 
+export default function Home() {
+  const [blocks, setBlocks] = useState<any[]>([]);
+  useEffect(() => { fetchLatestBlocks(20).then(setBlocks); }, []);
+  return (
+    <div>
+      <h1>Latest Blocks</h1>
+      <ul>
+        {blocks.map(b => (
+          <li key={b.height}>
+            <Link to={`/block/${b.height}`}>#{b.height}</Link> @ {new Date(b.timestamp).toLocaleTimeString()}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
